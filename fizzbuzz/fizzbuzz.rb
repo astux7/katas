c274d5265aa61901657f09866ab01e0bb07a6a42
astux7 <Asta.bevainyte@gmail.com>
2013-11-18 12:36:04 +0000

    Fizzbuzz solved

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,2 +1,25 @@
 def fizzbuzz(n)
+	return "FizzBuzz" if n % 3 == 0 && n % 5 == 0
+	return "Fizz" if n % 3 == 0
+	return "Buzz" if n % 5 == 0
+	return n
 end
+#write to the file
+def save_to_file
+	# open the file for writing
+  file = File.open(@filename, "w") 
+  # iterate over the array of students
+  @first_hundred.each do |line|
+  # puts student
+    file.puts line
+  end
+  file.close
+end
+
+
+require_relative './fizzbuzz'
+
+@filename = File.dirname(__FILE__) + '/first_hundred.txt'
+@first_hundred = (1..100).map { |n| fizzbuzz(n).to_s }
+save_to_file
+
