dd000addbc63f3291068d5dc37cab9fda1800781
astux7 <Asta.bevainyte@gmail.com>
2013-11-23 13:55:59 +0000

    fizbuzz game fix, not arg in methods was

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,16 +1,16 @@
 def devisible_by_3?(number)
   number % 3 == 0
 end
-def devisible_by_5?(number)
+def divisible_by_5?(number)
   number % 5 == 0
 end
 def devisible_by_15?(number)
-  devisible_by_3? && divisible_by_5?
+  devisible_by_3?(number) && divisible_by_5?(number)
 end
 
-def fizzbuzz(n)
-	return "FizzBuzz" if devisible_by_15?
-	return "Fizz" if devisible_by_3?
-	return "Buzz" if divisible_by_5?
-	return n
+def fizzbuzz(number)
+	return "FizzBuzz" if devisible_by_15?(number)
+	return "Fizz" if devisible_by_3?(number)
+	return "Buzz" if divisible_by_5?(number)
+	return number
 end
