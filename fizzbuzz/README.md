9c946f155565f863dd2bba32d73f1df7664a8445
astux7 <Asta.bevainyte@gmail.com>
2013-11-23 13:48:11 +0000

    fizzbuzz game method fixes

--- fizzbuzz/README.md
+++ fizzbuzz/README.md
@@ -9,3 +9,37 @@ ruby fizzbuzz.rb
 
 to run the test of it:
 rspec fizzbuzz_spec.rb
+
+
+OTHER EXAMPLE with the CLASS
+
+=begin
+################### Class ####################
+class FizzBuzz
+  def self.is_divisible_by_three?(number)
+      divisible_by?(3,number)
+  end
+  def self.is_divisible_by_five?(number)
+      divisible_by?(5,number)
+  end
+    def self.is_divisible_by_fifteen?(number)
+      divisible_by?(15,number)
+  end
+  def self.divisible_by?(divisor, number)
+    number % divisor == 0
+  end
+  def  self.fizzit(number)
+     return "FizzBuzz" if is_divisible_by_fifteen(number)
+     return "fizz" if is_divisible_by_three(number)
+     return "Buzz" if is_divisible_by_five(number)
+     number
+  end
+end
+
+irb > 1.upto(100) { |num| puts FizzBuzz.fizzit(num) }
+
+self.* class method
+
+gem - library incl in projects
+
+=end
--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -4,42 +4,13 @@ end
 def devisible_by_5?(number)
   number % 5 == 0
 end
+def devisible_by_15?(number)
+  devisible_by_3? && divisible_by_5?
+end
 
 def fizzbuzz(n)
-	return "FizzBuzz" if devisible_by_3? && is_divisible_by_five?
+	return "FizzBuzz" if devisible_by_15?
 	return "Fizz" if devisible_by_3?
-	return "Buzz" if is_divisible_by_5?
+	return "Buzz" if divisible_by_5?
 	return n
 end
-
-
-=begin
-###################
-class FizzBuzz
-  def self.is_divisible_by_three?(number)
-      divisible_by?(3,number)
-  end
-  def self.is_divisible_by_five?(number)
-      divisible_by?(5,number)
-  end
-    def self.is_divisible_by_fifteen?(number)
-      divisible_by?(15,number)
-  end
-  def self.divisible_by?(divisor, number)
-    number % divisor == 0
-  end
-  def  self.fizzit(number)
-     return "FizzBuzz" if is_divisible_by_fifteen(number)
-     return "fizz" if is_divisible_by_three(number)
-     return "Buzz" if is_divisible_by_five(number)
-     number
-  end
-end
-
-irb > 1.upto(100) { |num| puts FizzBuzz.fizzit(num) }
-
-self.* class method
-
-gem - library incl in projects
-
-=end
