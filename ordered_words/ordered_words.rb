8897afe533e196cfafef28c230d60b97c7beec11
astux7 <Asta.bevainyte@gmail.com>
2013-11-23 13:41:37 +0000

    ordered words resolved other simple way

--- ordered_words/ordered_words.rb
+++ ordered_words/ordered_words.rb
@@ -1,17 +1,11 @@
-def longer_words?(word)
-	0.upto(word.length-1) do |n|
-		if n < word.length-1
-			return  comparison_letters?(word,n) ? true : false
-		end 
-	end
-	return true
+def sort(word)
+	word.chars.sort.join
 end
 
-def comparison_letters?(word,pos)
-	return word[pos].downcase.bytes[0] > word[pos+1].downcase.bytes[0] ? false : true
+def same_word?(word, comparator)
+	word == comparator
 end
 
 def ordered?(word)
-	return true if word.length == 1
-	return longer_words?(word)
+	same_word?(sort(word), word)
 end
