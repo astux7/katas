d2637d2c39f0f2bac75941813945fbc514d6bc87
astux7 <Asta.bevainyte@gmail.com>
2013-11-30 18:12:05 +0000

    money to coins done

--- money_to_coins/money_to_coins.rb
+++ money_to_coins/money_to_coins.rb
@@ -2,58 +2,67 @@ def money_to_coins(amount)
   penny = [50,20,10,5,2,1]
   monets = [2,1]
   monets_penny = amount.split('.')
-  @returns=[]
+  @returnsp=[]
+  @returns =[]
+  @returnsm=[]
   #i have only coins
   if monets_penny[1] == nil
-    monets_penny[0].chomp('p')
-    split_amount_of(penny,monets_penny[0].to_i)
+    return_penny(monets_penny,penny,0)
   end
   #i have coins and monets
   if monets_penny[1] != nil
-    monets_penny[0] = monets_penny[0].gsub("£","")
-    split_amount_of(monets,monets_penny[0].to_i)
-    @returns << "."
-    split_amount_of(penny,monets_penny[1].to_i)
+    return_coins_money(monets_penny,monets,penny)
   end
-@returns
-# print_money(amount)
-  
+  print_money(amount).to_s
+end
+
+def return_coins_money(monets_penny,monets,penny)
+  monets_penny[0] = monets_penny[0].gsub("£","")
+  return_penny(monets_penny,penny,1)
+  @returns = []
+  split_amount_of(monets,monets_penny[0].to_i)
+  @returnsm = @returns
+end
+
+def return_penny(monets_penny,penny,iterator)
+  monets_penny[iterator].chomp('p')
+  split_amount_of(penny,monets_penny[iterator].to_i)
+  @returnsp = @returns 
 end
 
 def mixed?(amount)
   amount.include?(".")
 end
 
-def print_penny(money)
-   money.counts
+def prepare_print(array)
+  tmp = Hash.new(0)
+  array.each {|v| tmp[v] += 1 }
+  tmp = tmp.sort_by {|k,v| k}
+end
 
+def mur (array,monets =false)
+  b = prepare_print(array)
+  output = ""
+  b.each do |k, v|
+    output +=  (monets ?  "#{v}x#{k}p " : " #{v}x£#{k}")
+  end
+  output.chomp(' ')
 end
 
 def print_money(amount)
-
-    if mixed?(amount)
-    print 
-
-    end
-
-   print_penny
-
-    
-    
-
+  output = ""
+  output += mur(@returnsp,true)
+  output += mur(@returnsm) if mixed?(amount)
+  output
 end
 
-def  split_amount_of(money_template,money)
+def split_amount_of(money_template,money)
   return if money <= 0
-  money_template.each_with_index { |key,value| 
-  if money - key >= 0 
-     @returns << key
-     return split_amount_of(money_template, money - key)
+  money_template.each_with_index {|key,value| 
+  if money - key >= 0
+    @returns << key
+    return split_amount_of(money_template, money - key)
   end
   }
   @returns
 end
-
-print money_to_coins("£7.76")
-#print money_to_coins("£4.00")
-#print money_to_coins("£4.03")
